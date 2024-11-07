4a12d8152b1c3af5b81826021fce53297d0d2a70
mfarshad-fat <mfrokn@gmail.com>
2024-11-07 13:57:14 +0330

    Delete src directory

--- src/allocation/bootstrap.py
+++
@@ -1,9 +0,0 @@
-  
-# bootstrap.py
-from src.allocation.domain.entities import *
-from src.allocation.adapters.orm.database import engine , Base
-
-
-def init_db():
-    # ایجاد تمام جداول در پایگاه داده با استفاده از `Base.metadata`
-    Base.metadata.create_all(bind=engine)
