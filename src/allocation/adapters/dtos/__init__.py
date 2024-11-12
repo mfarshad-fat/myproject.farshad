dc276f7b8ecd79f364938909ea3e4370ce6fca7a
mfarshad-fat <mfrokn@gmail.com>
2024-11-12 16:50:29 +0330

    Initial commit

--- src/allocation/adapters/dtos/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .access_level_schemas import AcceclevelBase, Acceclevelread
-from .userjwt_schemas import UserjwtBase, Userjwtread , TokenData
-from .users_schemas import UsersBase, UsersOut, UsersUpdate
-from .books_schemas import BooksBase, BooksOut, BooksUpdate
-from .librarians_schemas import LibrariansBase, LibrariansOut, LibrariansUpdate
-from .reservations_schemas import ReservationsBase, ReservationsOut, ReservationsUpdate
-from .transactions_schemas import TransactionsBase, TransactionsOut, TransactionsUpdate
--- src/allocation/adapters/dtos/access_level_schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class AcceclevelBase(BaseModel) :
-    accec_name : str
-
-class Acceclevelread(AcceclevelBase) :
-    accec_id : str
-    class config:
-        orm_mode = True
